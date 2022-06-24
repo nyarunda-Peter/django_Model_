0d9cfd2da33537f13241e1bef4c1798a62108cc2
Nyarunda Peter <mosepete+1@gmail.com>
2022-06-24 10:36:27 +0300

    Added blog's Model, Views and Url

---
+++ .env/I4G001638H44/blog/views.py
@@ -0,0 +1,33 @@
+from msilib.schema import ListView
+from pyexpat import model
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import  DetailView
+from django.views.generic.edit import UpdateView
+
+import blog
+from blog.models import Post
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
